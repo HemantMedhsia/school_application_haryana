9990a7ed29f6dcb467eb8efe6448745e647b1ab2
vedansh tiwari <vt2855028@gmail.com>
2024-09-07 19:50:45 +0530

    Initial commit

--- School_app_Frontend/src/pages/StudentAdd.jsx
+++ School_app_Frontend/src/pages/StudentAdd.jsx
@@ -24,7 +24,7 @@ const StudentAdd = () => {
     dateOfBirth: "",
     category: "",
     religion: "",
-    caste: "",
+    // caste: "",
     mobileNumber: "",
     email: "",
     admissionDate: "",
