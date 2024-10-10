42080754c5e70173ff931973d587b3efec1465d5
Hemant Medhsia <142498656+MyCoddingJourney@users.noreply.github.com>
2024-10-10 16:40:09 +0530

    initial commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -56,6 +56,7 @@ const corsOptions = {
     },
     methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
+    Credentials: true,
 };
 
 app.use(cors(corsOptions));
