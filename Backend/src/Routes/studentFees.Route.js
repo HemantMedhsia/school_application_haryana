a51a60dd6f757a5482b6e589f903a5e420bfad64
vedansh tiwari <vt2855028@gmail.com>
2024-10-29 17:09:48 +0530

    initial commit

--- Backend/src/Routes/studentFees.Route.js
+++ Backend/src/Routes/studentFees.Route.js
@@ -3,6 +3,7 @@ import {
     addPaymentsAndDiscounts,
     getAllStudentFeeDetails,
     getDueFeeListPerClassMonth,
+    getStudentBillPerMonth,
     getStudentFeeDetails,
 } from "../Controller/studentFees.Controller.js";
 
@@ -11,6 +12,7 @@ const router = express.Router();
 router.post("/add-student-fee", addPaymentsAndDiscounts);
 router.get("/get-student-fee/:studentId", getStudentFeeDetails);
 router.post("/student-due-fee-per-class", getDueFeeListPerClassMonth);
-router.get("/get-all-student-fee",getAllStudentFeeDetails)
+router.get("/get-all-student-fee", getAllStudentFeeDetails);
+router.post("/get-student-bill-month", getStudentBillPerMonth);
 
 export { router as studentFeesRouter };
