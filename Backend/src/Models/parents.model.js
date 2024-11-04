4c89d60201d764d1c8cc69278101243f443811c0
vedansh tiwari <vt2855028@gmail.com>
2024-11-04 18:47:50 +0530

    commit after change is response

--- Backend/src/Models/parents.model.js
+++ Backend/src/Models/parents.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const parentSchema = new mongoose.Schema({
-    fatherName: {
+    s: {
         type: String,
         required: true,
     },
